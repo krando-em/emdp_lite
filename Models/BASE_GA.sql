89b95afb85b93166bffe4804cd12ebf97a96435d
Kyle Randolph <kylerandolph@essencemediacom.com>
2024-03-14 17:41:59 -0400

    col aliases

--- Models/BASE_GA.sql
+++ Models/BASE_GA.sql
@@ -1,17 +1,18 @@
 SELECT
-    date,
-    device,
-    campaignid,
-    campaign,
-    advertiserid,
-    advertiser,
-    adgroupid,
-    adgroup,
-    currency,
-    network,
-    impressions,
-    click,
-    media_cost,
-    interactions,
-    conversions
+    Day AS date,
+    Device AS device,
+    Campaign_id AS campaignid,
+    Campaign AS campaign,
+    "" AS advertiserid,
+    "" AS advertiser,
+    Ad_group_id AS adgroupid,
+    Ad_group AS adgroup,
+    Currency AS currency,
+    Network AS network,
+    Impressions AS impressions,
+    Clicks AS click,
+    Cost AS media_cost,
+    Interactions AS interactions,
+    Conversions AS conversions
+
 FROM {BASE_GA_TABLE}
